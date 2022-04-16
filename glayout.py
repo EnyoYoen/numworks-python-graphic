443464ed861a664b4418704d7342b41fa94f64be
Yoen <74349473+EnyoYoen@users.noreply.github.com>
2022-04-16 11:54:17 +0200

    Update glayout.py

--- glayout.py
+++ glayout.py
@@ -1,100 +1,102 @@
+from ion import keydown
+
 class GLayout:
-  def __init__(self,horizontal=True):
-    self._widgetList=[]
-    self._widgetListLen=0
-    self._widgetDictionnary={}
-    self._activateWidgetIndex=None
-    self._moveKey1=1
-    self._moveKey2=2
-    self._deactivateKey=6
-    self._executeKey=4
+ def __init__(self,h=True):
+  self._wl=[]
+  self._wll=0
+  self._wd={}
+  self._awi=None
+  self._mk1=1
+  self._mk2=2
+  self._dk=6
+  self._ek=4
 
-  def getWidgetList(self):
-    return self.self._widgetList
-  def getWidgetDictionnary(self):
-    return self._widgetDictionnary
-  def getWidgetListLen(self):
-    return self._widgetListLen
-  def getActivateIndex(self):
-    return self._activateWidgetIndex
-  def getKey(self,key):
-    if key==1:
-      key=self._moveKey1      
-    elif key==2:
-      key=self._moveKey2
-    elif key==3:
-      key=self._executeKey
-    elif key==4:
-      key=self._deactivateKey
-    return key
+ def getWidgetList(self):
+  return self._wl
+ def getWidgetDictionnary(self):
+  return self._wd
+ def getWidgetListLen(self):
+  return self._wll
+ def getActivateIndex(self):
+  return self._awi
+ def getKey(self,k):
+  if k==1:
+   k=self._mk1
+  elif k==2:
+   k=self._mk2
+  elif k==3:
+   k=self._ek
+  elif k==4:
+   k=self._dk
+  return k
 
-  def setKey(self,key,type):
-    if type==1:
-      self._moveKey1=key
-    elif type==2:
-      self._moveKey2=key
-    elif type==3:
-      self._executeKey=key
-    elif type==4:
-      self._deactivateKey=key
+ def setKey(self,k,t):
+  if t==1:
+   self._mk1=k
+  elif t==2:
+   self._mk2=k
+  elif t==3:
+   self._ek=k
+  elif t==4:
+   self._dk=k
 
-  def addWidget(self, widget, widgetName):
-    self._widgetDictionnary[widgetName]=widget
-    self._widgetList.append(widget)
-    self._widgetListLen+=1
+ def addWidget(self,w,wn):
+  self._wd[wn]=w
+  self._wl.append(w)
+  self._wll+=1
 
-  def removeWidget(self, widgetName):
-    widget=self._widgetDictionnary[widgetName]
-    self._widgetDictionnary.pop(widgetName)
-    self._widgetList.remove(widget)
-    self._widgetListLen-=1
-  
-  def keyPressed(self, key=None):
-    if self._widgetListLen!=0:
-      if key==None:
-        keys=[self._executeKey, self._deactivateKey, self._moveKey1, self._moveKey2]
+ def removeWidget(self,wn):
+  w=self._wd[wn]
+  self._wd.pop(wn)
+  self._wl.remove(w)
+  self._wll-=1
+ 
+ def keyPressed(self,k=None):
+  if self._wll!=0:
+   if k==None:
+    ks=[self._ek,self._dk,self._mk1,self._mk2]
+    for i in ks:
+     if keydown(i):
+      k=i
+      break
+
+   if k==self._ek:
+    if self._awi!=None:
+     cw=self._wl[self._awi]
+     return(cw.setClicked())
+
+   if k==self._mk1 or k==self._mk2:
+    if self._awi==None:
+     if k==self._mk1:
+      self._awi=0
+     else:
+      self._awi=self._wll-1
+    else:
+     self._setActivate(False)
+     if k==self._mk1:
+      if self._awi==0:
+       self._awi=self._wll-1
       else:
-        keys=[key]
+       self._awi-=1
+     else:
+      if self._awi==self._wll-1:
+       self._awi=0
+      else:
+       self._awi+=1
+    self._setActivate(True)
 
-      for key in keys:
-        if key==self._executeKey:
-          if self._activateWidgetIndex!=None:
-            currentWidget=self._widgetList[self._activateWidgetIndex]
-            if type(currentWidget)==GButton or type(currentWidget)==GInput:
-              return(currentWidget.setClicked())
+   if k==self._dk:
+    if self._awi!=None:
+     self._setActivate(False)
+     self._awi=None
 
-        if key==self._moveKey1 or key==self._moveKey2:
-          if self._activateWidgetIndex==None:
-            if key==self._moveKey1:
-              self._activateWidgetIndex=0
-            else:
-              self._activateWidgetIndex=self._widgetListLen-1
-          else:
-            self._setActivate(False)
-            if key==self._moveKey1:
-              if self._activateWidgetIndex==0:
-                self._activateWidgetIndex=self._widgetListLen-1
-              else:
-                self._activateWidgetIndex-=1
-            else:
-              if self._activateWidgetIndex==self._widgetListLen-1:
-                self._activateWidgetIndex=0
-              else:
-                self._activateWidgetIndex+=1
-          self._setActivate(True)
+ def display(self,s=True,st=0,e=-1,nd=[]):
+  if e<0:
+   e=self._wll
+  for i in range(st,e):
+   if not(i in nd):
+    self._wl[i].display(s)
 
-        if key==self._deactivateKey:
-          if self._activateWidgetIndex!=None:
-            self._setActivate(False)
-            self._activateWidgetIndex=None
-
-  def display(self,state=True,start=0,end=-1,notDisplay=[]):
-    if end<0:
-      end=self._widgetListLen
-    for i in range(start,end):
-      if not(i in notDisplay):
-        self._widgetList[i].display(state)
-
-  def _setActivate(self,state=True):
-    if self._activateWidgetIndex!=None:
-      self._widgetList[self._activateWidgetIndex].setActivate(state)
+ def _setActivate(self,s=True):
+  if self._awi!=None:
+   self._wl[self._awi].setActivate(s)
